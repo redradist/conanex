54528b39879fc848f5175cd0342f3f15132f6c8e
redradist <redradist@gmail.com>
2022-04-27 01:20:00 +0300

    Added regeneration of conanfile.txt

--- conanex/main.py
+++ conanex/main.py
@@ -48,9 +48,6 @@ def parse_args():
     install_parser.add_argument('--require-override', type=str, help='REQUIRE_OVERRIDE')
     install_parser.add_argument('path_or_reference', type=str)
     install_parser.add_argument('reference', type=str, nargs='?')
-    """
-    path_or_reference [reference]
-    """
     return parser.parse_args()
 
 
@@ -87,44 +84,137 @@ def build_create_args(args, tmpdirname, package):
     if args.e:
         new_args.append('-e')
         new_args.append(args.e)
-    if hasattr(args, '-e:b'):
+    if hasattr(args, 'e:b') and getattr(args, 'e:b'):
         new_args.append('-e:b')
-        new_args.append(getattr(args, '-e:b'))
-    if hasattr(args, '-e:h'):
+        new_args.append(getattr(args, 'e:b'))
+    if hasattr(args, 'e:h') and getattr(args, 'e:h'):
         new_args.append('-e:h')
-        new_args.append(getattr(args, '-e:h'))
-    if hasattr(args, '-e:o'):
+        new_args.append(getattr(args, 'e:h'))
+    if hasattr(args, 'e:o') and getattr(args, 'e:o'):
         new_args.append('-e:o')
-        new_args.append(getattr(args, '-e:o'))
+        new_args.append(getattr(args, 'e:o'))
     if args.pr:
         new_args.append('-pr')
         new_args.append(args.pr)
-    if hasattr(args, '-pr:b'):
+    if hasattr(args, 'pr:b') and getattr(args, 'pr:b'):
         new_args.append('-pr:b')
-        new_args.append(getattr(args, '-pr:b'))
-    if hasattr(args, '-pr:h'):
+        new_args.append(getattr(args, 'pr:b'))
+    if hasattr(args, 'pr:h') and getattr(args, 'pr:h'):
         new_args.append('-pr:h')
-        new_args.append(getattr(args, '-pr:h'))
+        new_args.append(getattr(args, 'pr:h'))
     if args.s:
         new_args.append('-s')
         new_args.append(args.s)
-    if hasattr(args, '-s:b'):
+    if hasattr(args, 's:b') and getattr(args, 's:b'):
         new_args.append('-s:b')
-        new_args.append(getattr(args, '-s:b'))
-    if hasattr(args, '-s:h'):
+        new_args.append(getattr(args, 's:b'))
+    if hasattr(args, 's:h') and getattr(args, 's:h'):
         new_args.append('-s:h')
-        new_args.append(getattr(args, '-s:h'))
+        new_args.append(getattr(args, 's:h'))
     if args.c:
         new_args.append('-c')
         new_args.append(args.c)
-    if hasattr(args, '-c:b'):
+    if hasattr(args, 'c:b') and getattr(args, 'c:b'):
         new_args.append('-c:b')
-        new_args.append(getattr(args, '-c:b'))
-    if hasattr(args, '-c:h'):
+        new_args.append(getattr(args, 'c:b'))
+    if hasattr(args, 'c:h') and getattr(args, 'c:h'):
         new_args.append('-c:h')
-        new_args.append(getattr(args, '-c:h'))
+        new_args.append(getattr(args, 'c:h'))
     new_args.append(tmpdirname)
     new_args.append(package)
+    return new_args
+
+
+def build_install_args(args, tmpfilename):
+    new_args = ['install']
+    if args.g:
+        new_args.append('-g')
+        new_args.append(args.g)
+    if hasattr(args, 'if') and getattr(args, 'if'):
+        new_args.append('-if')
+        new_args.append(getattr(args, 'if'))
+    if hasattr(args, 'of') and getattr(args, 'of'):
+        new_args.append('-of')
+        new_args.append(getattr(args, 'of'))
+    if args.build_require:
+        new_args.append('--build-require')
+    if args.no_imports:
+        new_args.append('--no-imports')
+        new_args.append(args.no_imports)
+    if args.lockfile_node_id:
+        new_args.append('--lockfile-node-id')
+        new_args.append(args.lockfile_node_id)
+    if args.build_require:
+        new_args.append('--build-require')
+    if args.j:
+        new_args.append('-j')
+        new_args.append(args.j)
+    if args.u:
+        new_args.append('-u')
+    if args.require_override:
+        new_args.append('--require-override')
+        new_args.append(args.require_override)
+    if args.m:
+        new_args.append('-m')
+        new_args.extend(args.m)
+    if args.mi:
+        new_args.append('-mi')
+        new_args.extend(args.mi)
+    if args.v:
+        new_args.append('-v')
+        new_args.extend(args.v)
+    if args.b:
+        new_args.append('-b')
+        new_args.extend(args.b)
+    if args.r:
+        new_args.append('-r')
+        new_args.append(args.r)
+    if args.l:
+        new_args.append('-l')
+        new_args.append(args.l)
+    if args.lockfile_out:
+        new_args.append('-lockfile-out')
+        new_args.append(args.lockfile_out)
+    if args.e:
+        new_args.append('-e')
+        new_args.append(args.e)
+    if hasattr(args, 'e:b') and getattr(args, 'e:b'):
+        new_args.append('-e:b')
+        new_args.append(getattr(args, 'e:b'))
+    if hasattr(args, 'e:h') and getattr(args, 'e:h'):
+        new_args.append('-e:h')
+        new_args.append(getattr(args, 'e:h'))
+    if hasattr(args, 'e:o') and getattr(args, 'e:o'):
+        new_args.append('-e:o')
+        new_args.append(getattr(args, 'e:o'))
+    if args.pr:
+        new_args.append('-pr')
+        new_args.append(args.pr)
+    if hasattr(args, 'pr:b') and getattr(args, 'pr:b'):
+        new_args.append('-pr:b')
+        new_args.append(getattr(args, 'pr:b'))
+    if hasattr(args, 'pr:h') and getattr(args, 'pr:h'):
+        new_args.append('-pr:h')
+        new_args.append(getattr(args, 'pr:h'))
+    if args.s:
+        new_args.append('-s')
+        new_args.append(args.s)
+    if hasattr(args, 's:b') and getattr(args, 's:b'):
+        new_args.append('-s:b')
+        new_args.append(getattr(args, 's:b'))
+    if hasattr(args, 's:h') and getattr(args, 's:h'):
+        new_args.append('-s:h')
+        new_args.append(getattr(args, 's:h'))
+    if args.c:
+        new_args.append('-c')
+        new_args.append(args.c)
+    if hasattr(args, 'c:b') and getattr(args, 'c:b'):
+        new_args.append('-c:b')
+        new_args.append(getattr(args, 'c:b'))
+    if hasattr(args, 'c:h') and getattr(args, 'c:h'):
+        new_args.append('-c:h')
+        new_args.append(getattr(args, 'c:h'))
+    new_args.append(tmpfilename)
     return new_args
 
 
@@ -186,9 +276,12 @@ def run():
                 else:
                     new_file_lines.append(str(line))
             with tempfile.TemporaryDirectory() as tmpdirname:
-                with tempfile.TemporaryFile(mode='w') as tmpfilename:
-                    tmpfilename.writelines(new_file_lines)
-                    argv = vars(args)
+                new_conanfile = os.path.join(tmpdirname, "conanfile.txt")
+                with open(new_conanfile, mode='w') as file:
+                    file.writelines(new_file_lines)
+                install_args = build_install_args(args, new_conanfile)
+                conan_install_command = [sys.executable, "-m", "conans.conan", *install_args]
+                with Popen(conan_install_command) as proc:
                     pass
     else:
         conan_command = [sys.executable, "-m", "conans.conan", *sys.argv[1:]]
