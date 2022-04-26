5f947ba8c9431b6ee5932f62604d8e39712eb01f
redradist <redradist@gmail.com>
2022-04-27 01:48:59 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'conans': ['*.txt'],
+        'conanex': ['*.txt'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
